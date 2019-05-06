1cf4ee2fa1342d72d29a9a4fb25d59a2619c594c
jianbinggouzi <360013263@qq.com>
2019-05-06 12:33:48 +0800

    app相关配置、常数

---
+++ app/src/main/java/com/pancaker/pancake/ConfigVar.java
@@ -0,0 +1,14 @@
+package com.pancaker.pancake;
+
+/**
+ * Created by jianbinggouzi on 19-5-6.
+ */
+
+public class ConfigVar {
+    //主机地址
+    public static String HOST_ADDR = "192.168.0.106";
+
+    public static String USER_NAME = "jianbinggouzi";
+
+    public static String PASSWORD = "123456";
+}
