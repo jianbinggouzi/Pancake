f6c4b19c61e7658838acfa0f7eb31eef3b69ec60
jianbinggouzi <360013263@qq.com>
2019-05-06 22:36:26 +0800

    Activity

--- app/src/main/java/com/pancaker/pancake/context/MainActivity.java
+++ app/src/main/java/com/pancaker/pancake/context/MainActivity.java
@@ -2,17 +2,60 @@ package com.pancaker.pancake.context;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import com.pancaker.pancake.R;
+import com.pancaker.pancake.custom.PaperDialog;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView shoujian;
+    //收件箱
+    private TextView receiveBox;
+    //杂货箱
+    private TextView goodsBox;
+    //弹出面板
+    private PaperDialog paperDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initActivity();
+    }
+
+    private void initActivity(){
+        receiveBox = (TextView) findViewById(R.id.receive_box);
+        goodsBox = (TextView) findViewById(R.id.good_box);
+        paperDialog = new PaperDialog(MainActivity.this,R.style.AlertDialogCustom);
+        paperDialog.createDialog();
+    }
+
+    private void initClickListener(){
+        receiveBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("receiveBox","receiveBox-message","receiveBox-remark");
+            }
+        });
+        goodsBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialog("goodsBox","goodsBox-message","messageBox-remark");
+            }
+        });
+    }
+
+    private void showDialog(String title,String message,String remark){
+        paperDialog.setTitle(title);
+        paperDialog.setMessage(message);
+        paperDialog.setRemark(remark);
+        paperDialog.setCheckOnClickListener("确认",new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                paperDialog.dismiss();
+            }
+        });
+        paperDialog.show();
     }
 }
