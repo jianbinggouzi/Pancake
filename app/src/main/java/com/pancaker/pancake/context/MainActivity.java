6fd887babe26611c946517473b97c41052ed2586
yfy <1090528900@qq.com>
2019-05-06 22:10:46 +0800

    =_=

--- app/src/main/java/com/pancaker/pancake/context/MainActivity.java
+++ app/src/main/java/com/pancaker/pancake/context/MainActivity.java
@@ -2,13 +2,16 @@ package com.pancaker.pancake.context;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.pancaker.pancake.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView shoujian;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
